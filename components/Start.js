125990c3cfd37ce332e70806eea3cf20bd459775
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-12-21 22:25:39 +0100

    Update Start.js

--- components/Start.js
+++ components/Start.js
@@ -7,8 +7,8 @@ export default class Start extends React.Component {
         <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Hello Screen1!</Text>
             <Button
-            title="Go to Screen 2"
-            onPress={() => this.props.navigation.navigate('Screen2')}
+            title="Go to Chat"
+            onPress={() => this.props.navigation.navigate('Chat')}
             />
         </View>
         )
