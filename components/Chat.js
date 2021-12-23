5c09033cb49e21c3f9a60ec8952a664e7e3713e4
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-12-23 16:08:56 +0100

    adding GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: 'This is a system message',
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+    
     render() {
         const { bgcolor } = this.props.route.params;
         return (
@@ -12,7 +49,16 @@ export default class Chat extends React.Component {
             alignItems:'center', 
             justifyContent:'center', 
             backgroundColor: bgcolor ? bgcolor : "white",}}>
-            <Text style={styles.title}>You'll see your chat here</Text>
+                <GiftedChat
+                renderBubble={this.renderBubble.bind(this)}
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+                />
+                { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+            {/* <Text style={styles.title}>You'll see your chat here</Text> */}
         </View>
         )
     }
