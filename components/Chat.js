ad98a54b34f07b375b920ed04a0ce782119ab875
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-12-23 13:49:28 +0100

    applying backgroundColor from Start.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 
 
 export default class Chat extends React.Component {
     render() {
+        const { bgcolor } = this.props.route.params;
         return (
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Hello Screen2!</Text>
+        
+        <View style={{
+            flex: 1,
+            alignItems:'center', 
+            justifyContent:'center', 
+            backgroundColor: bgcolor ? bgcolor : "white",}}>
+            <Text style={styles.title}>You'll see your chat here</Text>
         </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 45,
+        fontWeight: "bold",
+        color: 'white',
+        textAlign: "center",
+        padding: 20
+    },
+})
