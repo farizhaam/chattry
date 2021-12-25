9f908d5c3de37b253ce7ba6a53362d577968b9db
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-12-25 23:25:16 +0100

    no error mentioned, but chat not stored in firebase

--- components/Chat.js
+++ components/Chat.js
@@ -73,21 +73,21 @@ export default class Chat extends React.Component {
         const messages = [];
         // go through each document
         querySnapshot.forEach((doc) => {
-        // get the QueryDocumentSnapshot's data
-        let data = doc.data();
-        messages.push({
-            _id: data._id,
-            text: data.text,
-            createdAt: data.createdAt.toDate(),
-            user: {
-            _id: data.user._id,
-            name: data.user.name,
-            avatar: data.user.avatar
-            }
-        });
+            // get the QueryDocumentSnapshot's data
+            let data = doc.data();
+            messages.push({
+                _id: data._id,
+                text: data.text,
+                createdAt: data.createdAt.toDate(),
+                user: {
+                _id: data.user._id,
+                name: data.user.name,
+                avatar: data.user.avatar
+                }
+            });
         });
         this.setState({
-        messages: messages
+            messages: messages
         });
     };
 
